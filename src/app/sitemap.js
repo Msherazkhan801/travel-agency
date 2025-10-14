c46cdf180d8d3fcf2a6fa7364c779ae5a2d8a534
sheraz khan <sherazkhan801@gmail.com>
2025-10-14 17:13:18 +0500

    sitemap added

---
+++ src/app/sitemap.js
@@ -0,0 +1,34 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.usmannaimattravelagency.com',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1.0,
+    },
+    {
+      url: 'https://www.usmannaimattravelagency.com/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.usmannaimattravelagency.com/services',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.usmannaimattravelagency.com/contact',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.usmannaimattravelagency.com/faq',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+  ]
+}
