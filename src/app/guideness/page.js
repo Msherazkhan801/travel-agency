15d1181ce90202beb70861c5b751f760ce9de64c
sheraz khan <sherazkhan801@gmail.com>
2026-02-27 22:38:44 +0500

    cors aded

--- src/app/guideness/page.js
+++ src/app/guideness/page.js
@@ -68,6 +68,7 @@ export default function VideoGuides() {
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
+                  eferrerPolicy="strict-origin-when-cross-origin"
                 ></iframe>
               </div>
               <div className="p-5">
